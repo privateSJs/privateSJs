394e5fb77ad7e9f0781d8e21d6be8af7d7b3e656
Jarosław Stysiał <jarek9917@gmail.com>
2025-01-18 23:23:55 +0100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    images: {
+        unoptimized: true, // Konieczne dla statycznych stron bez optymalizacji obrazów
+    },
 };
 
 export default nextConfig;
