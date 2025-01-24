ea9cceb98ada24d46de4fa8549aa8881556c7db5
Jarosław Stysiał <jarek9917@gmail.com>
2025-01-24 07:23:36 +0100

    udpating repo

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true, // Konieczne dla statycznych stron bez optymalizacji obrazów
   },
-  basePath: isProd ? "/privateSJs" : "",
-  assetPrefix: isProd ? "/privateSJs" : "",
-  trailingSlash: true,
+  basePath: "/privateSJs",
+  assetPrefix: "/privateSJs/",
 };
 
 export default nextConfig;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
-import profile from "../assets/my-profile.jpg";
-import anacondaPicture from "../assets/anaconda-image.png";
-import pythonImage from "../assets/python-image.png";
-import dbImage from "../assets/db-image.png";
-import pythonImage2 from "../assets/python-image2.png";
+import profile from "../../public/assets/my-profile.jpg";
+import anacondaPicture from "../../public/assets/anaconda-image.png";
+import pythonImage from "../../public/assets/python-image.png";
+import dbImage from "../../public/assets/db-image.png";
+import pythonImage2 from "../../public/assets/python-image2.png";
 
 export const images = {
   profile,
