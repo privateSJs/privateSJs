903d31f53151d616d270297b2d5f2887efcd2773
Jarosław Stysiał <jarek9917@gmail.com>
2025-01-24 19:14:33 +0100

    test update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
         Witaj na mojej stronie!
       </h1>
       <p className="text-lg text-center text-gray-600">
-        To jest przykładowa strona aplikacji z rozwijanym paskiem bocznym.
+        To jest przykładowa strona aplikacji z rozwijanym paskiem bocznym. cos nowego akcja
       </p>
       <p className="text-md text-center text-gray-500">
         Możesz dostosować tę stronę, dodając nowe sekcje, komponenty i treści.
