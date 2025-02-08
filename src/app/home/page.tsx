0954f9c64742d30ab0307fed8b445081e553564d
Jarosław Stysiał <jarek9917@gmail.com>
2025-02-08 13:26:44 +0100

    update layout config

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,14 +13,14 @@ const Page = () => {
                 content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, , user-scalable=yes"/>
           <ParticlesBackground/>
           <div className=" absolute top-1/2 justify-center items-center ml-[20px] mr-[20px]">
-              <h1 className=" flex flex-col text-6xl font-bold text-left text-white mb-2 ml-3 ">
+              <h1 className=" flex flex-col text-4xl lg:text-6xl font-bold text-left text-white mb-2 ml-3 ">
                   <span>{bodyPage.welcome}</span>
                   <span>{bodyPage.introduce}
                       <span className="text-blue-300">{bodyPage.name}</span>
                   </span>
                   <span>{bodyPage.proffesion}</span>
               </h1>
-              <p className="flex text-lg text-md text-center text-gray-500 mt-10 ml-3 italic">
+              <p className="flex text-1xl lg:text-lg text-md text-center text-gray-500 mt-10 ml-3 italic">
                   ~ {bodyPage.peptalk} ~
               </p>
               <Link href="/contact">
