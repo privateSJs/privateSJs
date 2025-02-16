7b1d9d60f573f93f573dfe689fae643fea3ed0d5
Jarosław Stysiał <jarek9917@gmail.com>
2025-02-16 12:39:12 +0100

    css updated

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,42 +1,10 @@
 'use client'
-import {Chrono} from "react-chrono";
 
 
 const PortfolioPage = () => {
-    const items = [
-        {
-            title: "Technikum",
-            cardTitle: "Start projektu",
-            cardDetailedText: "Rozpoczęcie prac nad projektem.",
-        },
-        {
-            title: "Studia",
-            cardTitle: "Pierwsza wersja",
-            cardDetailedText: "Wdrożenie pierwszej wersji aplikacji.",
-        },
-        {
-            title: "Staż",
-            cardTitle: "Finalna wersja",
-            cardDetailedText: "Zakończenie prac i wdrożenie finalnej wersji.",
-        },
-    ];
-
     return (
         <div className="w-full h-full">
-            <Chrono
-                items={items}
-                mode="VERTICAL_ALTERNATING"
-                theme={{
-
-                }}
-                disableNavKey
-                buttonTexts={{
-                first: 'Jump to first',
-                last: 'Jump to last',
-                next: 'Next',
-                previous: 'Previous',
-            }} >
-            </Chrono>
+            tu bedzie chrono
         </div>
     )
 }
